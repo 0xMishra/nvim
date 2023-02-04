0f6754927ba09f2b4a8fb81317c8fe3d2d251ca1
mishra811 <mishrakundan073@gmail.com>
2023-02-04 10:39:39 +0530

    added comments

--- init.lua
+++ init.lua
@@ -400,7 +400,38 @@ require('lualine').setup {
 }
 
 -- Enable Comment.nvim
-require('Comment').setup()
+local status_ok, comment = pcall(require, "Comment")
+if not status_ok then
+  return
+end
+
+comment.setup {
+  pre_hook = function(ctx)
+    local U = require "Comment.utils"
+
+    local status_utils_ok, utils = pcall(require, "ts_context_commentstring.utils")
+    if not status_utils_ok then
+      return
+    end
+
+    local location = nil
+    if ctx.ctype == U.ctype.block then
+      location = utils.get_cursor_location()
+    elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
+      location = utils.get_visual_start_location()
+    end
+
+    local status_internals_ok, internals = pcall(require, "ts_context_commentstring.internals")
+    if not status_internals_ok then
+      return
+    end
+
+    return internals.calculate_commentstring {
+      key = ctx.ctype == U.ctype.line and "__default" or "__multiline",
+      location = location,
+    }
+  end,
+}
 
 -- Enable `lukas-reineke/indent-blankline.nvim`
 -- See `:help indent_blankline.txt`
@@ -409,17 +440,6 @@ require('indent_blankline').setup {
   show_trailing_blankline_indent = false,
 }
 
--- Gitsigns
--- See `:help gitsigns.txt`
-require('gitsigns').setup {
-  signs = {
-    add = { text = '+' },
-    change = { text = '~' },
-    delete = { text = '_' },
-    topdelete = { text = '‾' },
-    changedelete = { text = '~' },
-  },
-}
 
 -- [[ Configure Telescope ]]
 -- See `:help telescope` and `:help telescope.setup()`
