c1d91f0388fb9b8b215f23aedaef78ed95fabd10
mishra811 <mishrakundan073@gmail.com>
2023-12-03 19:34:22 +0530

    added conform.nvim plugin for formatting

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,20 +1,21 @@
 require("conform").setup({
 	formatters_by_ft = {
-
 		lua = { "stylua" },
 
 		python = { "isort", "black" },
 
-		-- Use a sub-list to run only the first available formatter
 		javascript = { { "prettierd", "prettier" } },
 		typescript = { { "prettierd", "prettier" } },
 		html = { { "prettierd", "prettier" } },
 		css = { { "prettierd", "prettier" } },
 		json = { { "prettierd", "prettier" } },
+		yaml = { { "prettierd", "prettier" } },
 
+		-- for reactjs files formatting
 		javascriptreact = { { "prettierd", "prettier" } },
 		typescriptreact = { { "prettierd", "prettier" } },
 	},
+
 	format_on_save = {
 		-- These options will be passed to conform.format()
 		timeout_ms = 500,
