0589d14cd71fbc8420a0cfb27564905eb2fba1c1
mishra811 <mishrakundan073@gmail.com>
2024-01-27 00:34:13 +0530

    added show definitions for coc servers

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -42,3 +42,17 @@ vim.keymap.set("n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>",
 vim.keymap.set("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>", { silent = true, noremap = true })
 vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>", { silent = true, noremap = true })
 vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>", { silent = true, noremap = true })
+
+-- Use K to show documentation in preview window for coc.nvim plugin
+function _G.show_docs()
+	local cw = vim.fn.expand("<cword>")
+	if vim.fn.index({ "vim", "help" }, vim.bo.filetype) >= 0 then
+		vim.api.nvim_command("h " .. cw)
+	elseif vim.api.nvim_eval("coc#rpc#ready()") then
+		vim.fn.CocActionAsync("doHover")
+	else
+		vim.api.nvim_command("!" .. vim.o.keywordprg .. " " .. cw)
+	end
+end
+
+vim.keymap.set("n", "<C-s>", "<CMD>lua _G.show_docs()<CR>", { silent = true })
