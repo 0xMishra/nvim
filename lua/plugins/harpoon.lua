111d5d9e2983457072036767e40aff1dc94c70bd
mishra811 <mishrakundan073@gmail.com>
2024-03-11 21:28:28 +0530

    removed telescope from harpoon

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -6,7 +6,7 @@ harpoon:setup({})
 vim.keymap.set("n", "<leader>m", function()
 	harpoon:list():append()
 end)
-vim.keymap.set("n", "<leader>q", function()
+vim.keymap.set("n", "<leader>hr", function()
 	harpoon.ui:toggle_quick_menu(harpoon:list())
 end)
 
@@ -42,28 +42,3 @@ end)
 vim.keymap.set("n", "<Tab>", function()
 	harpoon:list():next()
 end)
-
--- basic telescope configuration
-local conf = require("telescope.config").values
-local function toggle_telescope(harpoon_files)
-	local file_paths = {}
-	for _, item in ipairs(harpoon_files.items) do
-		table.insert(file_paths, item.value)
-	end
-
-	require("telescope.pickers")
-		.new({}, {
-			prompt_title = "Harpoon",
-			finder = require("telescope.finders").new_table({
-
-				results = file_paths,
-			}),
-			previewer = conf.file_previewer({}),
-			sorter = conf.generic_sorter({}),
-		})
-		:find()
-end
-
-vim.keymap.set("n", "<leader>q", function()
-	toggle_telescope(harpoon:list())
-end, { desc = "Open harpoon window" })
