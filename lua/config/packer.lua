00573a3fb4c576ec7351c2e7bda8ccd884d4e8df
mishra811 <mishrakundan073@gmail.com>
2024-04-10 00:24:30 +0530

    replaced telescope with fzf-lua

--- lua/config/packer.lua
+++ lua/config/packer.lua
@@ -79,22 +79,8 @@ require("packer").startup(function(use)
 		requires = { { "nvim-lua/plenary.nvim" } },
 	})
 
-	-- Fuzzy Finder (files, lsp, etc)
-	use({
-		"nvim-telescope/telescope.nvim",
-		branch = "0.1.x",
-		requires = { "nvim-lua/plenary.nvim" },
-	})
-
-	use("mfussenegger/nvim-lint") -- for linting
+	use("ibhagwan/fzf-lua")
 	use("stevearc/conform.nvim") -- for formatting
-
-	-- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
-	use({
-		"nvim-telescope/telescope-fzf-native.nvim",
-		run = "make",
-		cond = vim.fn.executable("make") == 1,
-	})
 
 	-- Add custom plugins to packer from ~/.config/nvim/lua/custom/plugins.lua
 	local has_plugins, plugins = pcall(require, "custom.plugins")
