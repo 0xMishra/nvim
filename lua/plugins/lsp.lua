c8592388e567300875f2d85ae344547832445775
mishra811 <mishrakundan073@gmail.com>
2024-04-20 05:51:28 +0530

    lsp changes

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,3 +1,4 @@
+-- LSP Keymaps & settings
 local lspconfig = require("lspconfig")
 local util = require("lspconfig.util")
 local cmp_nvim_lsp = require("cmp_nvim_lsp")
@@ -6,86 +7,57 @@ local mason = require("mason")
 local mason_lspconfig = require("mason-lspconfig")
 local mason_tool_installer = require("mason-tool-installer")
 
--- Add border to floating window
-vim.lsp.handlers["textDocument/signatureHelp"] =
-	vim.lsp.with(vim.lsp.handlers.hover, { border = "single", silent = true })
+--  This function gets run when an LSP connects to a particular buffer.
+local on_attach = function(_, bufnr)
+	local nmap = function(keys, func, desc)
+		if desc then
+			desc = "LSP: " .. desc
+		end
+		vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
+	end
 
-vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "single", silend = true })
+	nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+	nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
 
--- Make float window transparent start
-local set_hl_for_floating_window = function()
-	vim.api.nvim_set_hl(0, "NormalFloat", {
-		link = "Normal",
-	})
-	vim.api.nvim_set_hl(0, "FloatBorder", {
-		bg = "none",
-	})
+	nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
+
+	nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+	nmap("gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
+	nmap("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
+	nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+	nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+
+	-- See `:help K` for why this keymap
+	nmap("K", vim.lsp.buf.hover, "Hover Documentation")
+	nmap("<C-s>", vim.lsp.buf.signature_help, "Signature Documentation")
+
+	-- Lesser used LSP functionality
+	nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+
+	nmap("<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
+
+	nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
+	nmap("<leader>wl", function()
+		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+	end, "[W]orkspace [L]ist Folders")
+
+	-- Create a command `:Format` local to the LSP buffer
+	vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
+		vim.lsp.buf.format()
+	end, { desc = "Format current buffer with LSP" })
 end
 
-set_hl_for_floating_window()
-
-vim.api.nvim_create_autocmd("ColorScheme", {
-	pattern = "*",
-	desc = "Avoid overwritten by loading color schemes later",
-	callback = set_hl_for_floating_window,
-})
-
--- Make float window transparent end
-local on_attach = function(client, bufnr)
-	vim.keymap.set(
-		"n",
-		"K",
-		vim.lsp.buf.hover,
-		{ buffer = bufnr, desc = "Show documentation for what is under cursor" }
-	)
-	vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { buffer = bufnr, desc = "Smart rename" })
-	vim.keymap.set(
-		{ "n", "v" },
-		"gf",
-		vim.lsp.buf.code_action,
-
-		{ buffer = bufnr, desc = "See available code actions" }
-	)
-	vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { buffer = bufnr, desc = "Show diagnostics for line" })
-	vim.keymap.set(
-		"n",
-		"gr",
-		"<cmd>Telescope lsp_references<CR>",
-		{ buffer = bufnr, desc = "Show definition, references" }
-	)
-	vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = bufnr, desc = "Go to definition" })
-end
-
-local capabilities = cmp_nvim_lsp.default_capabilities()
-local signs = { Error = "E", Warn = "W", Hint = "H", Info = "I" }
-for type, icon in pairs(signs) do
-	local hl = "DiagnosticSign" .. type
-	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-end
-
-local function organize_imports()
-	local params = {
-		command = "_typescript.organizeImports",
-		arguments = { vim.api.nvim_buf_get_name(0) },
-	}
-
-	vim.lsp.buf.execute_command(params)
-end
-
-mason.setup({})
-
-mason_lspconfig.setup({
-	ensure_installed = {
-		"tsserver",
-		"lua_ls",
-		"html",
-		"cssls",
-		"emmet_ls",
-		"prismals",
-		"jsonls",
-	},
-	automatic_installation = true,
-})
+--  Add any additional override configuration in the following tables. They will be passed to
+--  the `settings` field of the server config.
+local servers = {
+	lua_ls = {},
+	html = {},
+	tsserver = {},
+	emmet_ls = {},
+	cssls = {},
+	prismals = {},
+	jsonls = {},
+}
 
 mason_tool_installer.setup({
 	ensure_installed = {
@@ -94,61 +66,64 @@ mason_tool_installer.setup({
 	},
 })
 
--- configure typescript server with plugin
-lspconfig["tsserver"].setup({
-	capabilities = capabilities,
-	on_attach = on_attach,
-	commands = {
-		OrganizeImports = {
-			organize_imports,
-			description = "Organize Imports",
-		},
-	},
-	root_dir = util.root_pattern(".git"),
+-- nvim-cmp supports additional completion capabilities, so broadcast that to servers
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
+
+-- Setup mason so it can manage external tooling
+mason.setup()
+
+mason_lspconfig.setup({
+	ensure_installed = vim.tbl_keys(servers),
 })
 
--- configure lua server (with special settings)
-lspconfig["lua_ls"].setup({
-	capabilities = capabilities,
-	on_attach = on_attach,
-	settings = { -- custom settings for lua
-		Lua = {
-			-- make the language server recognize "vim" global
-			diagnostics = {
-				globals = { "vim" },
+local function organize_imports()
+	local params = {
+		command = "_typescript.organizeImports",
+		arguments = { vim.api.nvim_buf_get_name(0) },
+	}
+	vim.lsp.buf.execute_command(params)
+end
+
+mason_lspconfig.setup_handlers({
+	function(server_name)
+		lspconfig[server_name].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+			settings = servers[server_name],
+		})
+	end,
+
+	["tsserver"] = function()
+		lspconfig["tsserver"].setup({
+			on_attach = on_attach,
+			capabilities = capabilities,
+			commands = {
+				OrganizeImports = {
+					organize_imports,
+					description = "Organize Imports",
+				},
 			},
-		},
-	},
-})
+			root_dir = util.root_pattern(".git"),
+		})
+	end,
 
--- configure html server
-lspconfig["html"].setup({
-	capabilities = capabilities,
-	on_attach = on_attach,
-})
-
--- configure css server
-lspconfig["cssls"].setup({
-	capabilities = capabilities,
-	on_attach = on_attach,
-})
-
--- configure emmet server
-lspconfig["emmet_ls"].setup({
-	capabilities = capabilities,
-	on_attach = on_attach,
-})
-
--- configure prisma server
-lspconfig["prismals"].setup({
-	capabilities = capabilities,
-	on_attach = on_attach,
-})
-
--- configure json server
-lspconfig["jsonls"].setup({
-	capabilities = capabilities,
-	on_attach = on_attach,
+	["lua_ls"] = function()
+		lspconfig["lua_ls"].setup({
+			on_attach = on_attach,
+			capabilities = capabilities,
+			settings = {
+				Lua = {
+					workspace = { checkThirdParty = false },
+					telemetry = { enable = false },
+					diagnostics = {
+						-- Get the language server to recognize the `vim` global
+						globals = { "vim" },
+					},
+				},
+			},
+		})
+	end,
 })
 
 vim.cmd([[
@@ -156,4 +131,4 @@ vim.cmd([[
 ]])
 
 -- for installing tailwindcss server via coc.nvim
-vim.cmd("command! InstallTailwindCSS :CocInstall @yaegassy/coc-tailwindcss3")
+vim.cmd("command! InstallCocTailwindCSS3 :CocInstall @yaegassy/coc-tailwindcss3")
