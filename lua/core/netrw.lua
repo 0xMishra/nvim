6bbdaa143a4f3f6a2deb7a733a03bd4de4757cb3
0xMishra <mishrakundan073@gmail.com>
2024-12-15 09:04:02 +0530

    fixed netrw bug

--- lua/core/netrw.lua
+++ lua/core/netrw.lua
@@ -2,7 +2,6 @@
 vim.cmd([[
   let g:netrw_winsize = 15
   let g:netrw_banner= 0
-  let g:netrw_list_hide = '^\.\./$,^\./$'
 
   hi! link netrwMarkFile Search
   let g:netrw_localcopydircmd = 'cp -r'
