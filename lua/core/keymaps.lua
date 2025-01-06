faec9421563f347fb9a847159bafa14a5df99a0c
0xMishra <mishrakundan073@gmail.com>
2025-01-07 01:09:07 +0530

    added keymaps to show coc docs

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -123,3 +123,19 @@ vim.api.nvim_create_autocmd("TermEnter", {
 vim.keymap.set("n", "<space>bf", function()
 	vim.lsp.buf.format()
 end, { silent = true })
+
+-- Coc keymaps
+-- Use K to show documentation in preview window for coc.nvim plugin
+function _G.show_docs()
+	local cw = vim.fn.expand("<cword>")
+
+	if vim.fn.index({ "vim", "help" }, vim.bo.filetype) >= 0 then
+		vim.api.nvim_command("h " .. cw)
+	elseif vim.api.nvim_eval("coc#rpc#ready()") then
+		vim.fn.CocActionAsync("doHover")
+	else
+		vim.api.nvim_command("!" .. vim.o.keywordprg .. " " .. cw)
+	end
+end
+
+vim.keymap.set("n", "<S-s>", "<CMD>lua _G.show_docs()<CR>", { silent = true })
