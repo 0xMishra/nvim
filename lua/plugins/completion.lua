07e10b9df8984983ff3518b9b227581c812042eb
0xMishra <mishrakundan073@gmail.com>
2025-05-19 13:47:32 +0530

    changed ui of cmp popup

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -36,7 +36,6 @@ return {
 		require("luasnip.loaders.from_vscode").lazy_load()
 		cmp.setup({
 			window = {
-				completion = cmp.config.window.bordered(border_opts),
 				documentation = cmp.config.window.bordered(border_opts),
 			},
 			completion = {
