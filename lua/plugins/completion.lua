74c1fb40250b600c4bb918eddaf8d6c46b9902eb
0xMishra <mishrakundan073@gmail.com>
2025-07-23 01:53:41 +0530

    removed buggy icons in blink cmp menu

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -12,6 +12,44 @@ return {
 			["<CR>"] = { "select_and_accept", "fallback" },
 			["<C-c>"] = { "cancel", "fallback" },
 		},
+		appearance = {
+			nerd_font_variant = "mono",
+			kind_icons = {
+				Text = "tx",
+				Method = "fn",
+
+				Function = "fn",
+
+				Constructor = "Co",
+
+				Field = "fi",
+				Variable = "va",
+				Property = "pr",
+
+				Class = "cl",
+				Interface = "if",
+				Struct = "st",
+				Module = "{}",
+
+				Unit = "Un",
+				Value = "vl",
+				Enum = "en",
+
+				EnumMember = "em",
+
+				Keyword = "kw",
+				Constant = "##",
+
+				Snippet = "sn",
+				Color = "co",
+				File = "fl",
+				Reference = "rf",
+				Folder = "di",
+				Event = "ev",
+				Operator = "op",
+				TypeParameter = "tp",
+			},
+		},
 		completion = {
 			documentation = { auto_show = true, auto_show_delay_ms = 10, window = { border = "single" } },
 		},
